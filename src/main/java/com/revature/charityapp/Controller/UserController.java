051b68bc9902a17d824d98b22ca703bd20372c35
shalujose <shalujosem0@gmail.com>
2019-09-23 09:25:33 +0530

    Changes committed

--- src/main/java/com/revature/charityapp/Controller/UserController.java
+++ src/main/java/com/revature/charityapp/Controller/UserController.java
@@ -6,48 +6,85 @@ import com.revature.Model.User;
 import com.revature.Services.UserService;
 
 public class UserController {
+	
 	public static String login(String email, String password) {
-        
-        String errorMessage = null;
-        UserService userservice = new UserService();
-        User user  = null;
-        try {
-            user = userservice.findByNameAndPassword(email, password);
-             if(user==null) {
-            	 throw new Exception("Invalid data");
-             }
-        } catch (Exception e) {
-            errorMessage = e.getMessage();
-        }       
-         
-        // Prepare JSON Object
-        String json = null;
-        Gson gson = new Gson();
-        if( user != null) {
-             json = gson.toJson(user);
-             
-        }
-        else if ( user == null ) {
-            JsonObject obj = new JsonObject();
-            obj.addProperty("errorMessage", errorMessage);
-            json = obj.toString();
-        }
-         
-         
-        return json;
-         
-    }
-     
-    public static void main(String[] args) {
-         
-        System.out.println("Test Case 1: Valid User");
-        String validUserJson = UserController.login("sha@gmail.com", "sha@123");
-        System.out.println(validUserJson);
-         
-        System.out.println("Test Case 2: Invalid User");
-        String invalidUserJson = UserController.login("invaliduser@gmail.com", "password");
-        System.out.println(invalidUserJson);
-         
-    }
+
+		String errorMessage = null;
+		UserService userservice = new UserService();
+		User user = null;
+		try {
+			user = userservice.findByNameAndPassword(email, password);
+			if (user == null) {
+				throw new Exception("Invalid data");
+			}
+		} catch (Exception e) {
+			errorMessage = e.getMessage();
+		}
+
+		// Prepare JSON Object
+		String json = null;
+		Gson gson = new Gson();
+		if (user != null) {
+			json = gson.toJson(user);
+
+		} else if (user == null) {
+			JsonObject obj = new JsonObject();
+			obj.addProperty("errorMessage", errorMessage);
+			json = obj.toString();
+		}
+
+		return json;
+
+	}
+
+	public static void main(String[] args) {
+
+		System.out.println("Test Case 1: Valid User");
+		String validUserJson = UserController.login("sha@gmail.com", "sha@123");
+		System.out.println(validUserJson);
+
+		System.out.println("Test Case 2: Invalid User");
+		String invalidUserJson = UserController.login("invaliduser@gmail.com", "password");
+		System.out.println(invalidUserJson);
+
+		User user=new User();
+		 String json =UserController.register(user);
+		 System.out.println(json);
+
+	}
+
+	 public static String register(User user) {
+	  
+	  String errorMessage = null;
+	  UserService userservice = new UserService();  
+	   try { 
+		   User us=new User();
+	   us.setName(user.getName());
+	   us.setGender(user.getGender());
+	   us.setAge(user.getAge());
+	   us.setEmail(user.getEmail());
+	   us.setPhone(user.getPhone());
+	   us.setPassword(user.getPassword());
+	   us = userservice.registerNow(user); 
+	   if (us == null) { 
+	   throw new Exception("Invalid data"); 
+	   } 
+	   } catch (Exception e) { 
+	   errorMessage =e.getMessage(); }
+	  
+	  // Prepare JSON Object 
+	  String json = null; 
+	  Gson gson = new Gson(); 
+	  if (user!= null) { json = gson.toJson(user);
+	  
+	  } 
+	  else if (user == null) { 
+	  JsonObject obj = new JsonObject();
+	  obj.addProperty("errorMessage", errorMessage); json = obj.toString(); }
+	  
+	  return json;
+	  
+	  }
+	 
 
 }
--- src/main/java/com/revature/charityapp/Servlet/RegisterServlet.java
+++ src/main/java/com/revature/charityapp/Servlet/RegisterServlet.java
@@ -8,8 +8,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.revature.charityapp.Model.User;
-import com.revature.charityapp.Services.UserService;
+import com.revature.Model.User;
+import com.revature.Services.UserService;
+
+//import com.revature.charity.Model.User;
+//import com.revature.charityapp.Services.UserService;
 
 /**
  * Servlet implementation class registerServlet
@@ -19,38 +22,56 @@ public class RegisterServlet extends HttpServlet {
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 	
-		String name = request.getParameter("name");
-		String gender=request.getParameter("gender");
-		int age = Integer.parseInt(request.getParameter("age"));	//parse the parameter as an int
-		String email = request.getParameter("email");
-		long phone=Long.parseLong(request.getParameter("phone"));
-		String password = request.getParameter("password");
-		PrintWriter out = response.getWriter();
-		out.print(name);
-		out.print(gender);
-		out.print(age);
-		out.print(email);
-		out.print(phone);
-		out.print(password);
-
 		
-		User user = new User();
-		user.setName(name);
-		user.setGender(gender);
-		user.setAge(age);
-		user.setEmail(email);
-		user.setPhone(phone);
-		user.setPassword(password);
+		  String name = request.getParameter("name"); 
+		  String gender=request.getParameter("gender"); 
+		  int age =Integer.parseInt(request.getParameter("age")); //parse the parameter as an
+		  String email = request.getParameter("email"); 
+		  long phone=Long.parseLong(request.getParameter("phone")); 
+		  String password =request.getParameter("password"); 
+		  PrintWriter out = response.getWriter();
+		  out.print(name); 
+		  out.print(gender); 
+		  out.print(age); 
+		  out.print(email);
+		  out.print(phone); 
+		  out.print(password);
+		  
+		  
+		  User user = new User(); 
+		  user.setName(name); 
+		  user.setGender(gender);
+		  user.setAge(age); 
+		  user.setEmail(email); 
+		  user.setPhone(phone);
+		  user.setPassword(password);
+		  
+		  System.out.println(user); 
+		  UserService userservice = new UserService(); 
+		  try {
+		  userservice.registerNow(user); 
+		  //response.sendRedirect("login.jsp"); 
+		  } catch(Exception e) { 
+			  e.printStackTrace();
+		  //response.sendRedirect("index.jsp?errorMessage=" + e.getMessage()); 
+		  }
+		  
 		
-		System.out.println(user);
-		UserService userservice = new UserService();
-		try {
-			userservice.registerNow(user);
-	        response.sendRedirect("login.jsp");
-		} catch (Exception e) {
-			e.printStackTrace();
-			response.sendRedirect("index.jsp?errorMessage=" + e.getMessage());
-		}
- 
+		/*
+		 * int age = Integer.parseInt(request.getParameter("age")); String
+		 * name=request.getParameter("name"); String
+		 * gender=request.getParameter("gender"); String
+		 * email=request.getParameter("email"); long
+		 * phone=Long.parseLong(request.getParameter("phone")); String
+		 * password=request.getParameter("password");
+		 * 
+		 * //2. call controller User user=new User(); user.setName(name);
+		 * user.setGender(gender); user.setAge(age); user.setEmail(email);
+		 * user.setPhone(phone); user.setPassword(password); String json =
+		 * UserController.register(user);
+		 * 
+		 * //3 . write PrintWriter out = response.getWriter(); out.write(json);
+		 * out.flush();
+		 */
 	}
 }
